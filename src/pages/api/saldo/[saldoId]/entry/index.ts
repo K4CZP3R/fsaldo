8ec9601fe31490c1299e9f999eb7a9dccafcbd54
K4CZP3R <kacper@kacperserewis.net>
2023-02-26 23:17:49 +0100

    split entry adding from updating saldo

---
+++ src/pages/api/saldo/[saldoId]/entry/index.ts
@@ -0,0 +1,61 @@
+import { getSessionAndUser } from "@/helpers/server-side.helper";
+import { ApiResponse } from "@/models/api-response.interface";
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+let prisma = new PrismaClient();
+
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ApiResponse<unknown>>
+) {
+
+    try {
+        const { user } = await getSessionAndUser(req, res)
+        const { saldoId } = req.query;
+
+        const saldo = await prisma.saldo.findFirstOrThrow({
+            where: {
+                id: saldoId as string,
+                userId: user.id
+            },
+            include: {
+                saldoEntry: true
+            }
+        });
+
+        switch (req.method) {
+            case "POST":
+                res.status(200).json({
+                    data: await prisma.saldo.update({
+                        where: {
+                            id: saldoId as string,
+                        },
+                        data: {
+                            saldoEntry: {
+                                create: [
+                                    req.body.saldoEntry
+                                ]
+                            }
+                        }
+                    })
+                });
+                break;
+            default:
+                res.status(405).json({
+                    message: "Method not allowed"
+                })
+        }
+
+
+
+    } catch (error) {
+        const message = error instanceof Error ? error.message : "Unknown error"
+        res.status(500).json({
+            message
+        })
+    }
+
+
+}
