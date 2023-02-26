8b0584044293a81f3800ed30f7d838f1014456e9
K4CZP3R <kacper@kacperserewis.net>
2023-02-26 22:44:32 +0100

    add saldo & saldo entry models

---
+++ src/models/saldo.model.ts
@@ -0,0 +1,14 @@
+export type Saldo = {
+    id: string;
+    name: string;
+    saldoEntry: SaldoEntry[] | undefined;
+
+}
+
+export type SaldoEntry = {
+    id: string;
+    name: string;
+    amount: number;
+    createdAt: Date;
+
+}
