241a58b2d6f15af2b9d36e825d5adaa6b4ef21c1
Kacper Serewiś <kacper@kacperserewis.net>
2023-02-27 09:33:02 +0100

    add loading and handle new entries

--- src/components/saldo-entry-row/saldo-entry-row.component.tsx
+++ src/components/saldo-entry-row/saldo-entry-row.component.tsx
@@ -9,7 +9,10 @@ import {
 } from "@tremor/react";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import { useCallback, useEffect, useState } from "react";
-import { updateSaldoEntry } from "@/helpers/client-side.helper";
+import {
+  addEntryToSaldo,
+  updateSaldoEntry,
+} from "@/helpers/client-side.helper";
 
 export type SaldoEntryRowProps = {
   saldoId: string;
@@ -22,21 +25,49 @@ export default function SaldoEntryRow(props: SaldoEntryRowProps) {
   const [name, setName] = useState(props.item.name);
   const [amount, setAmount] = useState(props.item.amount);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setName(props.item.name);
     setAmount(props.item.amount);
+
+    if (props.item.id === "") {
+      setEditMode(true);
+    }
   }, [props.item]);
 
   const callb = useCallback(() => {
-    updateSaldoEntry(props.saldoId, props.item.id, { name, amount })
-      .then((res) => {
-        props.onChange?.();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    setLoading(true);
+    if (props.item.id !== "") {
+      updateSaldoEntry(props.saldoId, props.item.id, { name, amount })
+        .then((res) => {
+          props.onChange?.();
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      addEntryToSaldo(props.saldoId, { name, amount })
+        .then((res) => {
+          props.onChange?.();
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }, [name, amount, props]);
+
+  if (loading) {
+    return (
+      <TableRow>
+        <TableCell>
+          <Text>Loading...</Text>
+        </TableCell>
+      </TableRow>
+    );
+  }
 
   if (editMode) {
     return (
