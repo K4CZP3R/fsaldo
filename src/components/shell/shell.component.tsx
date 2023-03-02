340125926ebd572e04bfb527395948628613b68e
Kacper Serewiś <kacper@kacperserewis.net>
2023-03-02 09:37:32 +0100

    optimize styling for smaller screens

--- src/components/shell/shell.component.tsx
+++ src/components/shell/shell.component.tsx
@@ -1,6 +1,8 @@
 import { Title, Text } from "@tremor/react";
 import Skeleton from "react-loading-skeleton";
 
+import styles from "./shell.module.css";
+
 export interface ShellProps {
   children: React.ReactNode;
   title: string;
@@ -9,15 +11,7 @@ export interface ShellProps {
 
 export default function Shell(props: ShellProps) {
   return (
-    <main
-      style={{
-        border: "1px solid rgb(229 231 235)",
-        borderWidth: "1px",
-        borderRadius: "0.5rem",
-        backgroundColor: "rgb(249 250 251)",
-        padding: "1.5rem",
-      }}
-    >
+    <main className={styles.main}>
       <Title>{props.title}</Title>
       <Text>{props.text || <Skeleton style={{ width: "25%" }} />}</Text>
 
