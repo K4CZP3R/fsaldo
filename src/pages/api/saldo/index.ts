cef931571d4d9e2b3951d379b5f0bacfc2f5fec0
Kacper Serewiś <kacper@kacperserewis.net>
2023-03-03 13:33:40 +0100

    formatting

--- src/pages/api/saldo/index.ts
+++ src/pages/api/saldo/index.ts
@@ -1,57 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { ApiResponse } from '@/models/api-response.interface'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { ApiResponse } from "@/models/api-response.interface";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import { getSessionAndUser } from '@/helpers/server-side.helper'
-import { PrismaClient } from '@prisma/client'
+import { getSessionAndUser } from "@/helpers/server-side.helper";
+import { PrismaClient } from "@prisma/client";
 
 let prisma = new PrismaClient();
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<ApiResponse<unknown>>
+  req: NextApiRequest,
+  res: NextApiResponse<ApiResponse<unknown>>
 ) {
+  try {
+    const { user } = await getSessionAndUser(req, res);
 
-    try {
-
-        const { user } = await getSessionAndUser(req, res)
-
-
-        switch (req.method) {
-            case "GET":
-                res.status(200).json({
-                    data: await prisma.saldo.findMany({
-                        where: {
-                            userId: user.id
-                        },
-                        include: {
-                            saldoEntry: true
-                        }
-                    })
-                })
-                break;
-            case "POST":
-                res.status(200).json({
-                    data: await prisma.saldo.create({
-                        data: {
-                            userId: user.id,
-                            name: req.body.name,
-                        }
-                    })
-                })
-                break;
-            default:
-                res.status(405).json({
-                    message: "Method not allowed"
-                })
-
-        }
-    } catch (error) {
-        const message = error instanceof Error ? error.message : "Unknown error"
-        res.status(500).json({
-            message
-        })
+    switch (req.method) {
+      case "GET":
+        res.status(200).json({
+          data: await prisma.saldo.findMany({
+            where: {
+              userId: user.id,
+            },
+            include: {
+              saldoEntry: true,
+            },
+          }),
+        });
+        break;
+      case "POST":
+        res.status(200).json({
+          data: await prisma.saldo.create({
+            data: {
+              userId: user.id,
+              name: req.body.name,
+              debitLimit: req.body.debitLimit,
+            },
+          }),
+        });
+        break;
+      default:
+        res.status(405).json({
+          message: "Method not allowed",
+        });
     }
-
-
+  } catch (error) {
+    const message = error instanceof Error ? error.message : "Unknown error";
+    res.status(500).json({
+      message,
+    });
+  }
 }
