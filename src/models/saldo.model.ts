5ed850c3862e6b2206a6371f9bbceb73042760b3
Kacper Serewiś <kacper@kacperserewis.net>
2023-03-03 13:33:31 +0100

    add debit limit to model

--- src/models/saldo.model.ts
+++ src/models/saldo.model.ts
@@ -1,20 +1,20 @@
 export type Saldo = {
-    id: string;
-    name: string;
-    createdAt: Date;
-    saldoEntry: SaldoEntry[];
-
-}
+  id: string;
+  name: string;
+  createdAt: Date;
+  saldoEntry: SaldoEntry[];
+  debitLimit?: number;
+};
 
 export type SaldoEntry = {
-    id: string;
-    name: string;
-    amount: number;
-    createdAt: string;
-}
+  id: string;
+  name: string;
+  amount: number;
+  createdAt: string;
+};
 
 export type SaldoEntryUpdate = {
-    name?: string;
-    amount?: number;
-    createdAt?: Date;
-}
+  name?: string;
+  amount?: number;
+  createdAt?: Date;
+};
