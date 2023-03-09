58bc1575e70c1fde0c79ff7077d8ead309e8e721
Kacper <kacper@kacperserewis.net>
2023-03-09 11:57:24 +0100

    wrap helpers in static func

--- src/helpers/string.helper.ts
+++ src/helpers/string.helper.ts
@@ -1,17 +1,17 @@
-export function valuta(value: number): string {
-  // Get current locale
-  return value.toLocaleString(window.navigator.language, {
-    style: "currency",
-    currency: "EUR",
-  });
-}
+import dayjs from "dayjs";
 
-export function date(value: Date): string {
-  // Get current locale
-  return value.toLocaleDateString(window.navigator.language);
-}
 
-export function strDate(value: string): string {
-  // Get current locale
-  return new Date(value).toLocaleDateString(window.navigator.language);
+export class StringHelper {
+  static valuta(value: number): string {
+    // Get current locale
+    return value.toLocaleString(window.navigator.language, {
+      style: "currency",
+      currency: "EUR",
+    });
+  }
+
+  static date(value: Date | dayjs.Dayjs): string {
+    let dayJsInstance = dayjs(value)
+    return dayJsInstance.format("DD-MM-YYYY");
+  }
 }
