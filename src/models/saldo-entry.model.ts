9a8294f28b8de69a5d256e3a0514d82afeb07b19
Kacper <kacper@kacperserewis.net>
2023-03-10 00:04:22 +0100

    formatting

--- src/models/saldo-entry.model.ts
+++ src/models/saldo-entry.model.ts
@@ -1,59 +1,60 @@
 import { StringHelper } from "@/helpers/string.helper";
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
 export interface DbSaldoEntry {
-    id: string;
-    name: string;
-    amount: number;
-    date: number;
-};
-
-export type DbSaldoEntryUpdate = {
-    name?: string;
-    amount?: number;
-    date?: number;
+  id: string;
+  name: string;
+  amount: number;
+  date: number;
 }
 
+export type DbSaldoEntryUpdate = {
+  name?: string;
+  amount?: number;
+  date?: number;
+};
+
 export class SaldoEntry {
-    id: string;
-    name: string;
-    amount: number;
-    date: dayjs.Dayjs;
+  id: string;
+  name: string;
+  amount: number;
+  date: dayjs.Dayjs;
 
-    constructor(id: string, name: string, amount: number, date: dayjs.Dayjs) {
-        this.id = id;
-        this.name = name;
-        this.amount = amount;
-        this.date = date;
-    }
+  active = true;
 
+  constructor(id: string, name: string, amount: number, date: dayjs.Dayjs) {
+    this.id = id;
+    this.name = name;
+    this.amount = amount;
+    this.date = date;
+  }
 
+  isAfter(other: SaldoEntry): boolean {
+    return this.date.isSame(other.date)
+      ? this.amount < other.amount
+      : this.date.isAfter(other.date);
+  }
+  static sortFunction(a: SaldoEntry, b: SaldoEntry): number {
+    return a.isAfter(b) ? 1 : -1;
+  }
 
-    isAfter(other: SaldoEntry): boolean {
-        return this.date.isSame(other.date) ? this.amount < other.amount : this.date.isAfter(other.date);
-    }
-    static sortFunction(a: SaldoEntry, b: SaldoEntry): number {
-        return a.isAfter(b) ? 1 : -1;
-    }
+  get stringDate(): string {
+    return StringHelper.date(this.date);
+  }
 
-    get stringDate(): string {
-        return StringHelper.date(this.date);
-    }
+  static fromDb(obj: DbSaldoEntry): SaldoEntry {
+    return new SaldoEntry(obj.id, obj.name, obj.amount, dayjs(obj.date));
+  }
+  static newNow(name: string, amount: number): SaldoEntry {
+    return new SaldoEntry("", name, amount, dayjs());
+  }
 
-    static fromDb(obj: DbSaldoEntry): SaldoEntry {
-        return new SaldoEntry(obj.id, obj.name, obj.amount, dayjs(obj.date));
-    }
-    static newNow(name: string, amount: number): SaldoEntry {
-        return new SaldoEntry("", name, amount, dayjs());
-    }
-
-    toDb(): DbSaldoEntry {
-        return {
-            id: this.id,
-            name: this.name,
-            amount: this.amount,
-            date: this.date.valueOf()
-        }
-    }
-
+  toDb(): DbSaldoEntry {
+    return {
+      id: this.id,
+      name: this.name,
+      amount: this.amount,
+      date: this.date.valueOf(),
+    };
+  }
 }
