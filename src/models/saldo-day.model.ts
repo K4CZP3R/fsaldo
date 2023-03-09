1ca07039d3ee590d06a8a72d37d3c5f61d37c4e9
Kacper <kacper@kacperserewis.net>
2023-03-09 23:42:48 +0100

    remove helper, implement in classes

---
+++ src/models/saldo-day.model.ts
@@ -0,0 +1,78 @@
+import dayjs from "dayjs";
+import { SaldoEntry } from "./saldo-entry.model";
+
+export class SaldoDay {
+  date: dayjs.Dayjs;
+
+  private entries: SaldoEntry[];
+
+  constructor(date: dayjs.Dayjs, entries?: SaldoEntry[]) {
+    this.date = date;
+    this.entries = entries ?? [];
+  }
+
+  addEntry(entry: SaldoEntry) {
+    if (!entry.date.isSame(this.date, "day"))
+      throw new Error("Entry date does not match day date");
+    this.entries.push(entry);
+  }
+
+  get dailySaldo(): number {
+    return this.entries.reduce((acc, e) => acc + e.amount, 0);
+  }
+
+  get transactions(): number {
+    return this.entries.length;
+  }
+}
+
+export type SaldoDayPresentable = {
+  id: string;
+  Saldo: number;
+  Date: string;
+  "Daily change": number;
+};
+
+export class SaldoDayRange {
+  start: dayjs.Dayjs;
+  end: dayjs.Dayjs;
+
+  days: SaldoDay[];
+
+  constructor(start: dayjs.Dayjs, end: dayjs.Dayjs) {
+    this.start = start;
+    this.end = end;
+    this.days = [];
+
+    for (let i = 0; i <= this.end.diff(this.start, "day"); i++) {
+      this.days.push(new SaldoDay(this.start.add(i, "day")));
+    }
+  }
+
+  get presentableDays(): SaldoDayPresentable[] {
+    let presentable: SaldoDayPresentable[] = [];
+
+    let saldo = 0;
+    for (let day of this.days) {
+      saldo += day.dailySaldo;
+      presentable.push({
+        id: day.date.format("YYYY-MM-DD"),
+        Saldo: saldo,
+        Date: day.date.format("YYYY-MM-DD"),
+        "Daily change": day.dailySaldo,
+      });
+    }
+
+    return presentable;
+  }
+
+  addEntry(entry: SaldoEntry): SaldoDay | undefined {
+    // Find the day
+    let day = this.days.find((d) => d.date.isSame(entry.date, "day"));
+    if (day) {
+      day.addEntry(entry);
+      return day;
+    }
+    return undefined;
+  }
+}
--- src/models/saldo.model.ts
+++ src/models/saldo.model.ts
@@ -1,3 +1,4 @@
+import { cloneDeep } from "lodash";
 import { DbSaldoEntry, SaldoEntry } from "./saldo-entry.model";
 
 export interface DbSaldo {
@@ -10,7 +11,7 @@ export interface DbSaldo {
 export class Saldo {
   id: string;
   name: string;
-  saldoEntry: SaldoEntry[];
+  private saldoEntry: SaldoEntry[];
   debitLimit?: number | undefined;
 
   constructor(
@@ -21,16 +22,41 @@ export class Saldo {
   ) {
     this.id = id;
     this.name = name;
-    this.saldoEntry = saldoEntry;
+    this.saldoEntry = saldoEntry.sort(SaldoEntry.sortFunction);
     this.debitLimit = debitLimit;
   }
 
-  get sortedSaldoEntry(): SaldoEntry[] {
-    return this.saldoEntry.sort(SaldoEntry.sortFunction);
+  public get saldoEntries(): SaldoEntry[] {
+    return cloneDeep(this.saldoEntry);
   }
+
 
   static fromDb(obj: DbSaldo): Saldo {
     return new Saldo(obj.id, obj.name, obj.saldoEntry.map(e => SaldoEntry.fromDb(e)), obj.debitLimit);
+  }
+
+  get endingSaldo(): number {
+    return this.saldoEntry.reduce((acc, e) => acc + e.amount, 0);
+  }
+
+  getSaldoTo(item: SaldoEntry): number {
+    // Calculate saldo to this item
+    let saldo = 0;
+
+    // Sort and deep copy
+    let entries = this.saldoEntries;
+
+    // Get index of item
+    let index = entries.findIndex((e) => e.id === item.id);
+
+    // Get all entries before this item
+    let entriesBefore = entries.slice(0, index + 1);
+
+    // Calculate saldo
+    entriesBefore.forEach((e) => {
+      saldo += e.amount;
+    });
+    return saldo;
   }
 
   toDb(): DbSaldo {
