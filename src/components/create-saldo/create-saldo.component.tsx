a350ecb4ef6aa7b2215b94b59e8f7b93c05257a0
Kacper <kacper@kacperserewis.net>
2023-03-09 18:23:30 +0100

    fix layout for create dialog

--- src/components/create-saldo/create-saldo.component.tsx
+++ src/components/create-saldo/create-saldo.component.tsx
@@ -2,7 +2,9 @@ import { createSaldo, updateSaldo } from "@/helpers/client-side.helper";
 import { Saldo } from "@/models/saldo.model";
 import { Card, Text, Title, TextInput, Button, Flex } from "@tremor/react";
 import { useCallback, useState } from "react";
+import { mutate } from "swr";
 import NumberInput from "../number-input/number-input.compontent";
+import styles from "./create-saldo.module.css";
 
 export type CreateSaldoProps = {
   onSubmit: (name: string) => void;
@@ -17,18 +19,18 @@ export default function CreateSaldo(props: CreateSaldoProps) {
   );
 
   function submit() {
+    props.onSubmit(name);
     if (updateInstead) {
       updateSaldo(props.saldo!.id, name, debitLimit).then(() =>
-        props.onSubmit(name)
+        mutate("/api/saldo")
       );
     } else {
-      createSaldo(name, debitLimit).then(() => props.onSubmit(name));
+      createSaldo(name, debitLimit).then(() => mutate("/api/saldo"));
     }
   }
 
   return (
     <Card
-      maxWidth="max-w-none"
       hFull={false}
       shadow={true}
       decoration=""
@@ -37,21 +39,27 @@ export default function CreateSaldo(props: CreateSaldoProps) {
     >
       <Title>{updateInstead ? "Update saldo" : "Create new saldo"}</Title>
 
-      <Flex>
-        <TextInput
-          placeholder="Name of the saldo"
-          maxWidth="max-w-sm"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+      <div className={styles.formContainer}>
+        <div>
+          <Text>Name:</Text>
+          <TextInput
+            placeholder="Name of the saldo"
+            maxWidth="max-w-sm"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
 
-        <NumberInput
-          maxWidth="max-w-sm"
-          value={debitLimit}
-          onlyPositive={true}
-          onChange={setDebitLimit}
-        />
-      </Flex>
+        <div>
+          <Text>Debit limit:</Text>
+          <NumberInput
+            maxWidth="max-w-sm"
+            value={debitLimit}
+            onlyPositive={true}
+            onChange={setDebitLimit}
+          />
+        </div>
+      </div>
       <Button
         text={updateInstead ? "Update" : "Create"}
         onClick={() => submit()}
